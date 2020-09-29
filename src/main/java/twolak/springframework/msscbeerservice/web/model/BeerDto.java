8fd7afe71622adbf00d0da644b3c867694266149
twolak88 <twolak88@gmail.com>
2020-09-29 23:19:20 +0200

    Add Bean validation for Beer #8

--- src/main/java/twolak/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/twolak/springframework/msscbeerservice/web/model/BeerDto.java
@@ -3,6 +3,10 @@ package twolak.springframework.msscbeerservice.web.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,17 +21,33 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-
+    
+    @Null
     private UUID id;
+    
+    @Null
     private Integer version;
-
+    
+    @Null
     private OffsetDateTime createdDate;
+    
+    @Null
     private OffsetDateTime lastModifiedDate;
-
+    
+    @NotBlank
     private String beerName;
+    
+    @NotNull
     private BeerStyleEnum beerStyle;
-
+    
+    @Positive
+    @NotNull
     private Long upc;
+    
+    @Positive
+    @NotNull
     private BigDecimal price;
+    
+    
     private Integer quantityOnHand;
 }
