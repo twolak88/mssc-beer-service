79e33e8fb0a14f8b8d91f0cdca796ec3f1b9eb92
twolak88 <twolak88@gmail.com>
2020-10-25 16:21:13 +0100

    refactoring

--- src/main/java/twolak/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/twolak/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -1,17 +1,18 @@
 package twolak.springframework.msscbeerservice.services.brewing;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import twolak.springframework.msscbeerservice.config.JmsConfig;
-import twolak.springframework.msscbeerservice.domain.Beer;
+import twolak.springframework.brewery.model.BeerDto;
 import twolak.springframework.brewery.model.events.BrewBeerEvent;
 import twolak.springframework.brewery.model.events.NewInventoryEvent;
+import twolak.springframework.msscbeerservice.config.JmsConfig;
+import twolak.springframework.msscbeerservice.domain.Beer;
 import twolak.springframework.msscbeerservice.repositories.BeerRepository;
-import twolak.springframework.brewery.model.BeerDto;
 
 /**
  *
@@ -21,21 +22,27 @@ import twolak.springframework.brewery.model.BeerDto;
 @RequiredArgsConstructor
 @Service
 public class BrewBeerListener {
-    
+
     private final BeerRepository beerRepository;
     private final JmsTemplate jmsTemplate;
-    
+
     @Transactional
     @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
     public void listen(BrewBeerEvent brewBeerEvent) {
         BeerDto beerDto = brewBeerEvent.getBeerDto();
-        
-        Beer beer = this.beerRepository.getOne(beerDto.getId());
-        beerDto.setQuantityOnHand(beer.getQuantityToBrew());
-        
-        log.debug("Brewed beer " + beer.getMinOnHand() + " : QOH: " + beerDto.getQuantityOnHand());
-        
-        NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
-        jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+
+        Optional<Beer> beerOptional = this.beerRepository.findById(beerDto.getId());
+        beerOptional.ifPresentOrElse((beer) -> {
+            beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+
+            log.debug("Brewed beer " + beer.getMinOnHand() + " : QOH: " + beerDto.getQuantityOnHand());
+
+            NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
+            jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+        }, () -> {
+            String errorMessage = "Beer not found! Id: " + beerDto.getId();
+            log.error(errorMessage);
+            throw new RuntimeException(errorMessage);
+        });
     }
 }
